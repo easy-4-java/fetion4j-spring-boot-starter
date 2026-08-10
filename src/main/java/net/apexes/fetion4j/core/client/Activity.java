da60337d536980b8ee57f8ffe3f5e42f9f91a34a
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:31:13 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/net/apexes/fetion4j/core/client/Activity.java
+++ src/main/java/net/apexes/fetion4j/core/client/Activity.java
@@ -18,7 +18,7 @@ import net.apexes.fetion4j.core.sipc.Sipc;
 /**
  * Represents an ongoing Fetion client activity (e.g. adding a buddy, chatting).
  *
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * @since 1.0.0
  */
 public abstract class Activity {
